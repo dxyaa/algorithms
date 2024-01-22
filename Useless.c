7a4a9067e6ad3b2e4f2d760180d1786d550f83da
Diya Sundeep <diyasundeep64@gmail.com>
2024-01-23 00:26:20 +0530

    stalinsort

--- Useless.c
+++ Useless.c
@@ -56,25 +56,76 @@ void bogosort(int arr[], int n)
     }
     return;
 }
-
+void stalinsort(int arr[], int arr2[], int n)
+{
+    p = 0;
+    arr2[0] = arr[0];
+    p++;
+    for (int i = 1; i < n; i++)
+    {
+        printf("\np:%d", p);
+        if (arr[i] > arr2[p - 1])
+        {
+            arr[p] = arr[i];
+            p++;
+            printf("\ninside p:%d", p);
+            printf("\n");
+            for (int i = 0; i < p; i++)
+            {
+                printf("%d\t", arr2[i]);
+            }
+        }
+    }
+    printf("\nsorted:\n");
+    for (int i = 0; i < p; i++)
+    {
+        printf("%d\t", arr2[i]);
+    }
+}
 void main()
 {
     int n, ch;
-    printf("\nEnter number of elements in array: ");
+    printf("\nNumber of elements enter karde: ");
     scanf("%d", &n);
     int arr[n];
-    printf("\nEnter array:\n");
+    int arr2[n];
+    printf("\nGo on,enter the array:\n");
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    printf("\nWhich sorting u wanna do?\n1. Bogo Sort\n");
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            flag = 1;
+            break;
+        }
+        else
+        {
+            flag = 0;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("\nAlready giving a sorted array? Nop bad game :(");
+        return;
+    }
+    printf("\nWhich sorting u wanna do?\n1. Bogo Sort\n2. Stalin sort \n : ");
     scanf("%d", &ch);
     switch (ch)
     {
     case 1:
         printf("\nBogosort hehe\n");
         bogosort(arr, n);
+        break;
+    case 2:
+        printf("Stalin sort oooh");
+        for (int i = 0; i < n; i++)
+        {
+            arr2[i] = 0;
+        }
+        stalinsort(arr, arr2, n);
         break;
     default:
         printf("Nope wrong choice bro\n");
